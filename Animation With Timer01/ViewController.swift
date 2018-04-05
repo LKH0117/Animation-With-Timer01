30a3fc93efb192b8836bc923d271cc4b7e351ad9
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-04-05 11:23:14 +0900

    timer 1~5 -> 5~1

--- Animation With Timer01/ViewController.swift
+++ Animation With Timer01/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     var count = 1
     var myTimer = Timer()
+    var check = true
+    var checkP = false
     @IBOutlet weak var imgV: UIImageView!
     
     @IBOutlet weak var mylbl: UILabel!
@@ -20,17 +22,36 @@ class ViewController: UIViewController {
         mylbl.text = String(count)
     }
     @IBAction func btS(_ sender: Any) {
-        myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(Animation), userInfo: nil, repeats: true)
+        //타이머 0.1초마다 계속됨
+ //       if checkP == false{
+ //           checkP = true
+             myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(Animation), userInfo: nil, repeats: true)
+ //       }else {
+ //           checkP = false
+ //           myTimer.invalidate()
+ //       }
     }
     
     @IBAction func btT(_ sender: Any) {
         myTimer.invalidate()
     }
     @objc func Animation() {
+        //카운트가 5일때 1로 돌아가면서 타이머가 돌아감
+    //  if count == 5{
+    //      count = 1
+    //  } else {
+    //      count = count + 1
+    //  }
+        //카운트가1일땐 증가하다가 5를만나면 내려가면서 타이머가돌아감
         if count == 5 {
-            count = 1
-        } else {
+            check = false
+        } else if count == 1 {
+            check = true
+        }
+        if check == true {
             count = count + 1
+        } else if check == false{
+            count = count - 1
         }
         imgV.image = UIImage(named: "frame\(count).png")
         mylbl.text = String(count)

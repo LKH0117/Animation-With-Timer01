2201c52801a9e83c197bcd36c7b5af1b9e5e732f
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-04-05 10:53:59 +0900

    timer

--- Animation With Timer01/ViewController.swift
+++ Animation With Timer01/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    var count = 1
+    var myTimer = Timer()
+    @IBOutlet weak var imgV: UIImageView!
+    
+    @IBOutlet weak var mylbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgV.image = UIImage(named:"frame1.png")
+        mylbl.text = String(count)
     }
-
+    @IBAction func btS(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(Animation), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func btT(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @objc func Animation() {
+        if count == 5 {
+            count = 1
+        } else {
+            count = count + 1
+        }
+        imgV.image = UIImage(named: "frame\(count).png")
+        mylbl.text = String(count)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
